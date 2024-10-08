775032d13cc8a9a9d3fe5f93a7f2b5ce92aa35ae
pandeyji9304 <ritikpandey9304@gmail.com>
2024-10-08 14:31:07 +0530

    updated driver modal

--- Models/Driver.js
+++ Models/Driver.js
@@ -8,7 +8,7 @@ const DriverSchema = new mongoose.Schema({
     role: { type: String, default: 'driver' },
     assignedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'LogisticsHead' },
     passwordResetRequired: { type: Boolean, default: true },
-    profileImage: {
+    image: {
         type: String,
         default: 'https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg', // Default image URL
         required: false
